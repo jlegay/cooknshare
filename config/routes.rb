46c964fb30945c81c7f5e3ca69aaffd9567c6bf1
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-07-18 15:19:17 +0200

    Home page : index recettes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'recettes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recettes
   resources :favorites, only: [:create, :index, :destroy]
