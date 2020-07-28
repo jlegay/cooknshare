ba23c7b0e223a55a40959fb9bbd2086ae386d1cc
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-07-28 17:41:46 +0200

    try to fix bug on prod

--- app/models/recette.rb
+++ app/models/recette.rb
@@ -1,12 +1,10 @@
 class Recette < ApplicationRecord
+  searchkick
   belongs_to :user
   has_one_attached :photo
   has_many :favorites, dependent: :destroy
 
   TYPESRECETTES = ['Entrée', 'Plat', 'Dessert', 'Apéro', 'Cocktail']
-
-  searchkick
-
   # include Filterable
   # scope :filter_by_type_recette, -> (type_recette) { where type_recette: type_recette }
 end
