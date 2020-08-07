9922ef612875158756ef53bdabcdc7c486d25093
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-08-07 13:38:54 +0200

    Add base category

--- app/models/recette.rb
+++ app/models/recette.rb
@@ -4,7 +4,7 @@ class Recette < ApplicationRecord
   has_one_attached :photo
   has_many :favorites, dependent: :destroy
 
-  TYPESRECETTES = ['Entrée', 'Plat', 'Dessert', 'Apéro', 'Cocktail']
+  TYPESRECETTES = ['Entrée', 'Plat', 'Dessert', 'Apéro', 'Cocktail', 'Base']
   # include Filterable
   # scope :filter_by_type_recette, -> (type_recette) { where type_recette: type_recette }
 end
