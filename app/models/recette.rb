729c58ae65454bd5dbad0fb6c1022f7113ebeb6a
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-08-28 10:52:08 +0200

    navbar on container

--- app/models/recette.rb
+++ app/models/recette.rb
@@ -4,6 +4,9 @@ class Recette < ApplicationRecord
   has_one_attached :photo
   has_many :commentaires, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  # avec options mise en forme texte : (ajouter as: :rich_text_area dans simple_form)
+  # has_rich_text :ingredients
+  # has_rich_text :preparation
 
   TYPESRECETTES = ['Entrée', 'Plat', 'Dessert', 'Apéro', 'Cocktail', 'Base']
   # include Filterable
